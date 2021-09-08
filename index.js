f1244681e028d26394a144ba8a4d67d8a19e25a7
Rashedul Hasan <Rashedul Hasan>
2021-09-09 02:25:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Array
+var friendsAge = [20, 21, 24, 25, 22, 17];
+var slice = friendsAge.slice(1,4);
+console.log(slice);
+
+
+
+// friendsAge[0] = 40;
+// console.log(friendsAge);
+// var bestFriend = friendsAge[2];
+// console.log(bestFriend);
+// console.log(friendsAge);
+// var position = friendsAge.indexOf(123);
+// var len = friendsAge.length; 
+// console.log(len);
+// friendsAge.pop();
+// console.log(friendsAge);
+// friendsAge.shift();
+// console.log(friendsAge);
+
+
+
+
---
+++ loop.js
@@ -0,0 +1,30 @@
+//While
+// var num = 1;
+// while(num<5){
+//     console.log("Hello World",num);
+//     num++;
+// }
+
+//For-Loop
+// var nums = [12, 34, 56, 34, 65, 23];
+// for(var i=0;i<nums.length;i++){
+//     console.log(nums[i]);
+// }
+
+//Switch
+
+var num = 5;
+switch(num){
+    case 10:
+    case 5:
+        console.log("10");
+        break;
+    case 20:
+        console.log("10");
+        break;
+    case 30:
+        console.log("10");
+        break;
+    default:
+        console.log("Not Found");
+}
